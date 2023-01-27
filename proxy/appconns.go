d39022969dbc40455f2f2dc87c72492f21863a57
232425wxy <1378406814@mail.nwpu.edu.cn>
2023-01-27 20:08:32 +0800

    开始完善节点

--- proxy/appconns.go
+++ proxy/appconns.go
@@ -1,1 +1,33 @@
 package proxy
+
+import (
+	"github.com/232425wxy/meta--/abci"
+	"github.com/232425wxy/meta--/common/service"
+	"github.com/232425wxy/meta--/log"
+)
+
+type AppConns struct {
+	service.BaseService
+	consensus   *AppConnConsensus
+	txspool     *AppConnTxsPool
+	application abci.Application
+}
+
+func NewAppConns(application abci.Application, logger log.Logger) *AppConns {
+	conns := &AppConns{application: application}
+	conns.BaseService = *service.NewBaseService(nil, "AppConns")
+	conns.SetLogger(logger.New("module", "proxy_conns"))
+	return conns
+}
+
+func (conns *AppConns) Consensus() *AppConnConsensus {
+	return conns.consensus
+}
+
+func (conns *AppConns) TxsPool() *AppConnTxsPool {
+	return conns.txspool
+}
+
+func (conns *AppConns) Start() error {
+
+}
